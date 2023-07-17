cf09306d98e30f55fb417eb1637ec582d0ccdab2
Frances Tran <frances.tran1600@gmail.com>
2023-07-17 09:52:57 -0500

    Initial Commit

---
+++ aboutMe/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  aboutMe
+//
+//  Created by Scholar on 7/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var aboutMe = false
+    var body: some View {
+        ZStack{
+            Color(.systemGray)
+                .ignoresSafeArea()
+            VStack(alignment: .leading, spacing: 20.0) {
+                Text("Hello my name is Frances Tran")
+                    .multilineTextAlignment(.center)
+                Image("camping")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
+                Button(action: {aboutMe.toggle()})
+                {
+                    Text("About me")
+                }
+                if aboutMe {
+                    Text("I enjoy camping, running, drawing, and listening to music. I also like eating CAVA and ice cream.")
+                }
+                font(.title2)
+                .buttonStyle(.borderedProminent)
+                .tint(.blue)
+            }
+        }
+    }
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+}
